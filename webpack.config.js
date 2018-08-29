d7052ccbe1abe04335cace1402e15c83f222265b
Jack <johnmccormick1118@gmail.com>
2018-08-29 17:58:41 -0500

    First commit.

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "../docs/")
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "docs")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "postcss-loader"
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.svg$/,
+        exclude: /img/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "icon/",
+              publicPath: "icon/"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "img/",
+              publicPath: "img/"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Default",
+      template: "./src/html-templates/index-template.html",
+      chunks: ["app"],
+      filename: "index.html"
+    }),
+    new CleanWebpackPlugin(["dist"])
+  ]
+};
